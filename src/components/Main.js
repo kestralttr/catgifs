369cffeff4c35082b38ea231c836568585bc627a
Alex Bennett <kestralttr@gmail.com>
2017-06-10 00:22:40 -0700

    adds button for new GIF

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,7 @@ class Main extends React.Component {
     this.state = {
 
     };
+    this.generateNewGIF = this.generateNewGIF.bind(this);
   }
 
   componentDidMount() {
@@ -28,12 +29,31 @@ class Main extends React.Component {
 
   }
 
+  generateNewGIF(e) {
+    e.preventDefault();
+
+    let rand = Math.floor(Math.random()*25);
+    let gifURL;
+    let img = document.getElementById("main-gif");
+
+    fetch("http://api.giphy.com/v1/gifs/search?q=cat&api_key=dc6zaTOxFJmzC")
+    .then((response) => {
+      return response.json();})
+    .then((json) => {
+      console.log("here!");
+      gifURL = json.data[rand].images.original.url;
+      console.log(gifURL);
+      img.src = gifURL;
+    });
+  }
+
 
   render() {
     return(
       <div>
         <h1 id="main-h1">Cat GIFs!</h1>
-        <img id="main-gif"/>
+        <img id="main-gif" alt="Cat Pic"/><br></br>
+        <button onClick={this.generateNewGIF} id="new-gif-button">New GIF!</button>
       </div>
     );
   }
